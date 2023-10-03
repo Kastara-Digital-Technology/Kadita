bce71f5bcc107e347d09f5f96aafd02a5fd64417
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-10-03 23:17:40 +0700

    Initial Commit (#1)

---
+++ src/Kadita.h
@@ -0,0 +1,25 @@
+/*
+ *  KaditaIoT.h
+ *
+ *  Kadita lib header
+ *  Created on: 2023. 4. 3
+ */
+
+#pragma once
+
+#ifndef KADITA_LIB_H
+#define KADITA_LIB_H
+
+#include "Arduino.h"
+
+// custom external sensor: will be updated soon
+#ifdef SENSOR_MODULE_H
+#endif
+
+#include "input-module.h"
+#include "output-module.h"
+
+#include "timer-duration.h"
+#include "timer-task.h"
+
+#endif
