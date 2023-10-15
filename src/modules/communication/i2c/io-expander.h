be6a206fd1ad661b53e56165b2850a4e6cc0c551
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-10-15 14:10:24 +0700

    add pcf8574 io expander module

---
+++ src/modules/communication/i2c/io-expander.h
@@ -0,0 +1,22 @@
+/*  
+ *  io-expander.h
+ *
+ *  io expander module header
+ *  Created on: 2023. 4. 3
+ */
+
+#ifndef KADITA_IO_EXPANDER_H
+#define KADITA_IO_EXPANDER_H
+
+#include "Arduino.h"
+#include "../../io/input-module.h"
+#include "../../io/output-module.h"
+#include "PCF8574.h"
+
+class IOExpander : public PCF8574 {
+private:
+    using PCF8574::PCF8574;
+public:
+};
+
+#endif  // KADITA_IO_EXPANDER_H
